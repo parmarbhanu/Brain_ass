dd3d926a2cd9f2241e2bc6bd085a335676f4887a
unknown <parmarsinghbhanu@gmail.com>
2024-03-06 00:29:09 +0530

    final:

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -2,18 +2,26 @@ const jwt = require('jsonwebtoken');
 
 const authMiddleware = (req, res, next) => {
   // console.log(req.cookies.token)
-  const token = req.cookies.token;
-  if (!token) {
+  // const token = req.cookies.token;
+  // if (!token) return res.status(401).json({ errorMessage: "Unauthorized" });
+  // const decoded  = jwt.verify(token, process.env.JWT_SECRET);
+  // next();
+  if (!token) 
     return res.status(401).json({ message: 'Authentication failed' });
-  }
+  
 
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
     req.userId = decoded.userId;
     next();
-  } catch (error) {
+  } 
+  catch (error) {
     return res.status(401).json({ message: 'Authentication failed' });
   }
 };
 
 module.exports = authMiddleware;
+
+
+
+  
